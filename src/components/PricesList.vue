71b69f1ee1d35880c2826fbd64d9fb3a6c0534e4
Serhii Simonov <ss@kosmotech.com.ua>
2022-07-17 18:10:17 +0300

    Prices json DONE

--- src/components/PricesList.vue
+++ src/components/PricesList.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="prices">
             <div class="container">
-                <h1>Ціни</h1>
+                <h1>{{ pricesListInfoData.title }}</h1>
 
 
                 <!-- <carousel :items-to-show="1.5">
@@ -16,33 +16,33 @@
                 </carousel> -->
 
 
-                <div class="our_prices_slider">
+                <div v-for="(prices, index) in pricesListInfoData.prices" :key="index" class="our_prices_slider">
 
 
                     <div class="our_prices_slider_item">
 
-                        <p>60<span>хвилин</span></p>
+                        <p>{{ prices.prices_time }}<span>{{ prices.prices_time_name }}</span></p>
                         
-                        <h5>Консультація у <span>власниці центру</span></h5>
+                        <h5>{{ prices.prices_title }}<span>{{ prices.prices_title_color }}</span></h5>
         
                         <ul>
-                            <li>Lorem ipsum dolor sit amet</li>
-                            <li>Lorem ipsum dolor sit amet</li>
-                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>{{ prices.prices_about_first }}</li>
+                            <li>{{ prices.prices_about_second }}</li>
+                            <li>{{ prices.prices_about_third }}</li>
                         </ul>
         
                         <div class="slider_prices_wrap">
                             <div class="slider_prices_first_wrap">
-                                <p>500₴</p>
-                                <p>Онлайн</p>
+                                <p>{{ prices.prices_price_online }}</p>
+                                <p>{{ prices.prices_price_online_title }}</p>
                             </div>
                             <div class="slider_prices_second_wrap">
-                                <p>700₴</p>
-                                <p>Офлайн</p>
+                                <p>{{ prices.prices_price_offline }}</p>
+                                <p>{{ prices.prices_price_offline_title }}</p>
                             </div>
                         </div>
         
-                        <button type="button" class="btn btn_red">Записатися</button>
+                        <button type="button" class="btn btn_red">{{ prices.button }}</button>
 
                     </div>
 
@@ -106,7 +106,7 @@
 
                         <p>60<span>хвилин</span></p>
                         
-                        <h5>наступна <span>консультація</span></h5>
+                        <h5>Наступна <span>консультація</span></h5>
         
                         <ul>
                             <li>Lorem ipsum dolor sit amet</li>
@@ -138,7 +138,7 @@
 </template>
 
 <script>
-
+import axios from 'axios'
 // import 'vue3-carousel/dist/carousel.css';
 // import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
 
@@ -150,5 +150,21 @@ export default {
 //         Pagination,
 //         Navigation,
 //   },
+    data () {
+            return {
+                pricesListInfoData: [],
+            }
+        },
+        created() {
+            axios
+                .get('/data/pricesInfo.json')
+                .then(resp=>{
+                    this.pricesListInfoData = resp.data
+                })
+                .catch(err=>{
+                    this.$toast.error(err)
+                })
+
+        }
 }
 </script>

7708accdd987ceeddc7d5b9b5278fe7d3f01f0a1
Serhii Simonov <ss@kosmotech.com.ua>
2022-07-17 17:56:31 +0300

    Prices css Done

--- src/components/PricesList.vue
+++ src/components/PricesList.vue
@@ -1,12 +1,55 @@
 <template>
     <section id="prices">
             <div class="container">
+                <h1>Ціни</h1>
+
+
+                <!-- <carousel :items-to-show="1.5">
+                    <slide v-for="slide in 10" :key="slide">
+                        {{ slide }}
+                    </slide>
+
+                    <template #addons>
+                        <navigation />
+                        <pagination />
+                    </template>
+                </carousel> -->
+
 
                 <div class="our_prices_slider">
 
+
                     <div class="our_prices_slider_item">
 
-                        <p>60хвилин</p>
+                        <p>60<span>хвилин</span></p>
+                        
+                        <h5>Консультація у <span>власниці центру</span></h5>
+        
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                        </ul>
+        
+                        <div class="slider_prices_wrap">
+                            <div class="slider_prices_first_wrap">
+                                <p>500₴</p>
+                                <p>Онлайн</p>
+                            </div>
+                            <div class="slider_prices_second_wrap">
+                                <p>700₴</p>
+                                <p>Офлайн</p>
+                            </div>
+                        </div>
+        
+                        <button type="button" class="btn btn_red">Записатися</button>
+
+                    </div>
+
+
+                    <!-- <div class="our_prices_slider_item">
+
+                        <p>60<span>хвилин</span></p>
                         
                         <h5>Консультація у власниці центру</h5>
         
@@ -33,7 +76,7 @@
 
                     <div class="our_prices_slider_item">
 
-                        <p>60хвилин</p>
+                        <p>60<span>хвилин</span></p>
                         
                         <h5>Перша <span>консультація</span></h5>
         
@@ -60,6 +103,8 @@
                     </div>
 
                     <div class="our_prices_slider_item">
+
+                        <p>60<span>хвилин</span></p>
                         
                         <h5>наступна <span>консультація</span></h5>
         
@@ -83,7 +128,7 @@
         
                         <button type="button" class="btn">Записатися</button>
 
-                    </div>
+                    </div> -->
 
 
                 </div>
@@ -93,7 +138,17 @@
 </template>
 
 <script>
+
+// import 'vue3-carousel/dist/carousel.css';
+// import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+
 export default {
-    name: 'PricesList'
+    name: 'PricesList',
+//     components: {
+//         Carousel,
+//         Slide,
+//         Pagination,
+//         Navigation,
+//   },
 }
 </script>

9dbe301810a3d53133f33f85db344dc8e5096643
Serhii Simonov <ss@kosmotech.com.ua>
2022-07-19 22:30:25 +0300

    turn_to_us json DONE

--- src/components/MainPages/TurnToUs.vue
+++ src/components/MainPages/TurnToUs.vue
@@ -2,10 +2,25 @@
     <section id="turn_to_us">
             <div class="container">
 
-                <h1>Коли до нас звертаються</h1>
+                <h1>{{turnToUsListInfoData.title}}</h1>
 
                 <div class="turn_to_us_lists_wrap">
-                    <div class="turn_to_us_img_wrap">
+
+
+                    <div v-for="(turn_to_us, index) in turnToUsListInfoData.turn_to_us" :key="index" class="turn_to_us_img_wrap" :class="{turn_to_us_img_wrap_reverse: index % 2 !== 0}">
+
+                        <div class="wrap_img">
+                            <img :src="require(`@/assets/images/`+turn_to_us.img)" :alt="turn_to_us.turn_to_us_name+` — Фото`">
+                        </div>
+
+                        <div class="turn_to_us_text_wrap">
+                            <h4>{{ turn_to_us.turn_to_us_name }}</h4>
+                            <p>{{ turn_to_us.turn_to_us_text }}</p>
+                        </div>
+                        
+                    </div>
+                    
+                    <!-- <div class="turn_to_us_img_wrap">
 
                         <div class="wrap_img">
                             <img src="@/assets/images/turn_to_us_anxiety.jpg" alt="">
@@ -13,7 +28,7 @@
 
                         <div class="turn_to_us_text_wrap">
                             <h4>Відчуваєш тривогу в режимі нонстоп?</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, </p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
                         </div>
 
                     </div>
@@ -68,19 +83,38 @@
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam </p>
                         </div>
                     
-                    </div>
+                    </div> -->
+
                 </div>
                 <div class="turn_to_us_bottom_info">
-                    <p>Вихід один - записатися до спеціаліста та почати жити вільно!</p>
+                    <p>{{ turnToUsListInfoData.bottom_info }}</p>
                 </div>
-                <button type="button" class="btn btn_red">Записатися</button>
+                <button type="button" class="btn btn_red">{{ turnToUsListInfoData.button }}</button>
             </div>
 
         </section>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    name: 'TurnToUs'
+    name: 'TurnToUs',
+    data () {
+        return {
+            turnToUsListInfoData: [],
+        }
+    },
+    created() {
+        axios
+            .get('/data/turnToUsInfo.json')
+            .then(resp=>{
+                this.turnToUsListInfoData = resp.data
+            })
+            .catch(err=>{
+                this.$toast.error(err)
+            })
+
+    }
 }
 </script>

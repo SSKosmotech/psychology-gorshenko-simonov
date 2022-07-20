56060d3ac8ea534da010550c5caa904e10c68660
Serhii Simonov <ss@kosmotech.com.ua>
2022-07-20 11:38:49 +0300

    why_we json DONE

--- src/components/MainPages/WhyWe.vue
+++ src/components/MainPages/WhyWe.vue
@@ -2,7 +2,7 @@
     <section id="why_we">
             <div class="container">
             
-                <h1>Чому саме ми?</h1>
+                <h1>{{ whyWeListInfoData.title }}</h1>
 
                 <div class="why_we_list_wrap">
 
@@ -10,32 +10,35 @@
                         
                         <div class="our_lists_detail_wrap">
                             <ul>
-                                <li><b>75%</b> людей залишаються з нами й надалі</li>
-                                <li><b>24/7</b> - час, який можемо тобі присвятити</li>
-                                <li>Ми <b>персонально</b> під тебе складемо план нашої роботи</li>
-                                <li>Наші фахівці вивчають <b>нові підходи</b> до терапії</li>
-                                <li><b>Європейські</b> методи терапії</li>
+                                <template v-for="(list, index) in whyWeListInfoData.why_we_lists" :key="index">
+                                    <li v-html="list.list_item"></li>
+                                </template>
+                                <!-- <li><b>75%</b> людей залишаються з нами й надалі</li> -->
+                                <!-- <li><b>24/7</b> - час, який можемо тобі присвятити</li> -->
+                                <!-- <li>Ми <b>персонально</b> під тебе складемо план нашої роботи</li> -->
+                                <!-- <li>Наші фахівці вивчають <b>нові підходи</b> до терапії</li> -->
+                                <!-- <li><b>Європейські</b> методи терапії</li> -->
                             </ul>
-                            <a href="">
-                                Детальніше
+                            <a :href="whyWeListInfoData.link" target="_blank">
+                                {{ whyWeListInfoData.link_text }}
                             </a>
                         </div>
 
-                        <div class="wrap_img">
-                            <img src="@/assets/images/why-we-reasons.jpg" alt="why_we_pic - фото">
+                        <div v-for="(img, index) in whyWeListInfoData.image" :key="index" class="wrap_img">
+                            <img :src="require(`@/assets/images/`+img.img_reasons)" :alt="whyWeListInfoData.title+`_01 — Фото`">
                         </div>
 
                     </div>
 
                     <div class="interesting_offer">
                         
-                        <div class="wrap_interesting_img">
-                            <img src="@/assets/images/why-we-interesting-offer.jpg" alt="interesting-offer — фото">
+                        <div v-for="(img, index) in whyWeListInfoData.image" :key="index" class="wrap_interesting_img">
+                            <img :src="require(`@/assets/images/`+img.img_interesting_offer)" :alt="whyWeListInfoData.title+`_02 — Фото`">
                         </div>
 
                         <div class="interesting_offer_text_wrap">
-                            <p><b>Зменшили вартість</b> наших послуг на час військових дій. <b>Все буде Україна!</b></p>
-                            <p>Нехай панічні атаки та тривожність йдуть слідом за орками</p>
+                            <p v-html="whyWeListInfoData.interesting_offer_01"></p>
+                            <p>{{ whyWeListInfoData.interesting_offer_02 }}</p>
                         </div>
                         
                     </div>
@@ -46,7 +49,25 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    name: 'WhyWe'
+    name: 'WhyWe',
+    data () {
+        return {
+            whyWeListInfoData: [],
+        }
+    },
+    created() {
+        axios
+            .get('/data/whyWeInfo.json')
+            .then(resp=>{
+                this.whyWeListInfoData = resp.data
+            })
+            .catch(err=>{
+                this.$toast.error(err)
+            })
+
+    }
 }
 </script>

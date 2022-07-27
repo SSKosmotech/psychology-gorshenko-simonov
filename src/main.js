56f344f3f403d78879b0e4672e86089c68ac7895
Serhii Simonov <ss@kosmotech.com.ua>
2022-07-28 01:03:15 +0300

    Delete comments in scss

--- src/main.js
+++ src/main.js
@@ -4,10 +4,6 @@ import router from './router'
 import Toaster from '@meforma/vue-toaster'
 import BackToTop from 'vue-backtotop'
 
-
-// import '@/assets/css/reset.scss'
-// import '@/assets/css/fonts_style.css'
-// import '@/assets/css/contacts.scss'
 import '@/assets/css/hamburger.scss'
 import '@/assets/css/main.scss'
 import '@/assets/css/respons.scss'
